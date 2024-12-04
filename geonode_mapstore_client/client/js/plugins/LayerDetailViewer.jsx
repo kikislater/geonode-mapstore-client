eef1d520f03a99079375ad948e04f358768f8bed
Suren <dsuren1@gmail.com>
2024-12-04 18:49:38 +0530

    Fix import for LayerDetailView plugin (#1919)

--- geonode_mapstore_client/client/js/plugins/LayerDetailViewer.jsx
+++ geonode_mapstore_client/client/js/plugins/LayerDetailViewer.jsx
@@ -26,7 +26,18 @@ import { hashLocationToHref } from '@js/utils/SearchUtils';
 import { mapSelector } from '@mapstore/framework/selectors/map';
 import { parsePluginConfigExpressions } from '@js/utils/MenuUtils';
 import tooltip from '@mapstore/framework/components/misc/enhancers/tooltip';
-import tabComponents from '@js/plugins/detailviewer/tabComponents';
+
+import DetailsLocations from '@js/components/DetailsPanel/DetailsLocations';
+import DetailsAssets from '@js/components/DetailsPanel/DetailsAssets';
+import DetailsAttributeTable from '@js/components/DetailsPanel/DetailsAttributeTable';
+import DetailsLinkedResources from '@js/components/DetailsPanel/DetailsLinkedResources';
+
+const tabComponents = {
+    'attribute-table': DetailsAttributeTable,
+    'linked-resources': DetailsLinkedResources,
+    'locations': DetailsLocations,
+    'assets': DetailsAssets
+};
 
 const Button = tooltip(GNButton);
 
